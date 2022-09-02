9b923394c97ef62ee30b01d00e6dc920dbf83700
Finn <34718806+FinnDore@users.noreply.github.com>
2022-09-02 23:47:13 +0100

    added terminal div

---
+++ src/components/terminal.tsx
@@ -0,0 +1,7 @@
+export function Terminal() {
+    return (
+        <div className="bg-zinc min-h-[20rem] h-1/3  border-white/25 border-t bg-black">
+            <div className="border-white/25 border-b px-4 py-2 flex justify-between bg-zinc-900">Terminal</div>
+        </div>
+    );
+}
