f1ab24e0afa481e369176718f95df11033253312
Wyzlle <101371689+wyzlle@users.noreply.github.com>
2022-09-03 05:23:52 +0000

    feat: add xterm.css

--- src/components/terminal-area.tsx
+++ src/components/terminal-area.tsx
@@ -1,14 +1,20 @@
 import React, { useEffect } from "react";
 import { Terminal } from "xterm";
-
+import { FitAddon } from "xterm-addon-fit";
+import "xterm/css/xterm.css";
 export function TerminalArea() {
   useEffect(() => {
-    const term = new Terminal();
+    const term = new Terminal({ cursorBlink: true, cols: 90, rows: 10 });
+    const fitAddon = new FitAddon();
+    term.loadAddon(fitAddon);
     const ws = new WebSocket("wss://h-production.up.railway.app");
     term.open(document.getElementById("terminal") ?? document.body);
     term.write("\r");
     term.onData((data) => {
       ws.send(data);
+    });
+    term.onResize(() => {
+      fitAddon.fit();
     });
     ws.addEventListener("message", (event) => {
       term.write(event.data);
