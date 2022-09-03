e2f788b3d0a7e2a25bb959db06a9be5137e278ff
Finn <34718806+FinnDore@users.noreply.github.com>
2022-09-03 11:36:54 +0100

    cleaned up effect

--- src/components/terminal-area.tsx
+++ src/components/terminal-area.tsx
@@ -4,44 +4,41 @@ import { FitAddon } from 'xterm-addon-fit';
 import 'xterm/css/xterm.css';
 
 const useTerminal = () => {
-    const term = useRef<Terminal | undefined>(undefined);
-    const fitAddon = useRef<FitAddon | undefined>(undefined);
+    const term = useRef<{ terminal?: Terminal; fitAddon?: FitAddon; wsMessage?: EventListener }>({});
+
     useEffect(() => {
-        term.current = new Terminal({ cursorBlink: true, theme: {} });
-        fitAddon.current = new FitAddon();
-        term.current.loadAddon(fitAddon.current);
+        term.current.terminal = new Terminal({ cursorBlink: true, theme: {} });
+        term.current.fitAddon = new FitAddon();
+        const terminal = term.current.terminal;
+        const fitAddon = term.current.fitAddon;
+
+        term.current.terminal.loadAddon(term.current.fitAddon);
 
         const ws = new WebSocket('wss://h-production.up.railway.app');
 
-        term.current.open(document.getElementById('terminal') ?? document.body);
-        term.current.write('\r');
-        term.current.onData((data) => {
-            console.log('data');
-            ws.send(data);
-        });
-        term.current.onResize(() => {
-            fitAddon.current?.fit();
-        });
-        ws.addEventListener('message', (event) => {
-            term.current?.write(event.data);
-        });
+        terminal.open(document.getElementById('terminal') ?? document.body);
+        terminal.write('\r');
+
+        terminal.onData((data) => ws.send(data));
+        terminal.onResize(() => term.current.fitAddon?.fit());
+
+        const handleMessage = (event: MessageEvent<string | Uint8Array>) => term.current.terminal?.write(event.data);
+        ws.addEventListener('message', handleMessage, {});
 
         return () => {
-            console.log('aaaa', term);
-            term.current?.dispose();
-
-            term.current = undefined;
-            fitAddon.current?.dispose();
-            fitAddon.current = undefined;
+            terminal.dispose();
+            fitAddon.dispose();
+            ws.removeEventListener('message', handleMessage, {});
         };
     }, []);
 
-    return { term, fitAddon };
+    return term.current;
 };
 
 export function TerminalArea() {
-    const { fitAddon } = useTerminal();
-    fitAddon.current?.fit();
+    const term = useTerminal();
+    term.fitAddon?.fit();
+
     return (
         <div className="bg-zinc  border-white/25 border-t bg-black overflow-auto min-h-[20rem] h-2/3 max-h-2/3">
             <div className="border-white/25 border-b px-4 py-2 flex justify-between bg-zinc-900 text-xs">Terminal</div>
