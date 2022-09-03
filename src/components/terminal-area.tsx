0ec75df9c87eac208109a9a04cf6148ec9e7333f
Finn <34718806+FinnDore@users.noreply.github.com>
2022-09-03 10:57:31 +0100

    fixed useeffect

--- src/components/terminal-area.tsx
+++ src/components/terminal-area.tsx
@@ -1,33 +1,47 @@
-import React, { useEffect } from "react";
-import { Terminal } from "xterm";
-import { FitAddon } from "xterm-addon-fit";
-import "xterm/css/xterm.css";
+import { useEffect, useRef } from 'react';
+import { Terminal } from 'xterm';
+import { FitAddon } from 'xterm-addon-fit';
+import 'xterm/css/xterm.css';
+
+const useTerminal = () => {
+    const term = useRef<Terminal | undefined>(undefined);
+    useEffect(() => {
+        term.current = new Terminal({ cursorBlink: true, cols: 90, rows: 10 });
+        const fitAddon = new FitAddon();
+        term.current.loadAddon(fitAddon);
+
+        const ws = new WebSocket('wss://h-production.up.railway.app');
+
+        term.current.open(document.getElementById('terminal') ?? document.body);
+        term.current.write('\r');
+        term.current.onData((data) => {
+            ws.send(data);
+        });
+        term.current.onResize(() => {
+            fitAddon.fit();
+        });
+        ws.addEventListener('message', (event) => {
+            term.current?.write(event.data);
+        });
+
+        return () => {
+            term.current?.dispose();
+            term.current = undefined;
+        };
+    }, []);
+
+    return term;
+};
+
 export function TerminalArea() {
-  useEffect(() => {
-    const term = new Terminal({ cursorBlink: true, cols: 90, rows: 10 });
-    const fitAddon = new FitAddon();
-    term.loadAddon(fitAddon);
-    const ws = new WebSocket("wss://h-production.up.railway.app");
-    term.open(document.getElementById("terminal") ?? document.body);
-    term.write("\r");
-    term.onData((data) => {
-      ws.send(data);
-    });
-    term.onResize(() => {
-      fitAddon.fit();
-    });
-    ws.addEventListener("message", (event) => {
-      term.write(event.data);
-    });
-  });
-  return (
-    <div className="bg-zinc min-h-[20rem] h-1/3  border-white/25 border-t bg-black">
-      <div className="border-white/25 border-b px-4 py-2 flex justify-between bg-zinc-900">
-        Terminal
-      </div>
-      <div id="terminal" />
-    </div>
-  );
+    useTerminal();
+    console.log('render');
+    return (
+        <div className="bg-zinc min-h-[20rem] h-1/3  border-white/25 border-t bg-black">
+            <div className="border-white/25 border-b px-4 py-2 flex justify-between bg-zinc-900">Terminal</div>
+            <div id="terminal" />
+        </div>
+    );
 }
 
 export default TerminalArea;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ export default function document() {
             <Head>
                 <meta name="description" content="To space!... i guess" />
                 <link rel="icon" href="/favicon.ico" />
-                <meta property="og:image" content="/logo.png" />
+                <meta property="og:image" content="/icon.png" />
                 <meta name="theme-color" content="#f54251" />
                 <meta name="twitter:card" content="summary_large_image"></meta>
             </Head>
