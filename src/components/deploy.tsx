fa18e7c88987c096c56825cf52da7f1ba6937ca3
Wyzlle <101371689+wyzlle@users.noreply.github.com>
2022-09-04 10:13:10 +0000

    add deploy

--- src/components/deploy.tsx
+++ src/components/deploy.tsx
@@ -1,8 +1,18 @@
 import { AlertDialog, Content, Overlay, Portal, Trigger } from '@radix-ui/react-alert-dialog';
 import { atom, useAtom, useAtomValue } from 'jotai';
 import { Logo } from '../ds/logo';
-
+import { useWs } from '@/hooks/use-ws';
 const deetsOpenAtom = atom(false);
+
+const deploy = async (token: string) => {
+    const ws = useWs()
+    if (ws) {
+        const handleOpen = () => {
+            ws.send(JSON.stringify({ "type": "termIn", "data": `hop auth login --token ${token} && hop projects switch && hop deploy` }));
+        };
+        ws.addEventListener('open', handleOpen, {});
+    }
+};
 
 const DeployBtn = () => {
     return (
@@ -29,6 +39,17 @@ const DeetsOverlay = () => {
                 <Content>
                     <div className="flex flex-col bg-zinc-900 w-1/3 h-1/3 fixed m-auto top-[50%] left-[50%] -translate-y-1/2 -translate-x-1/2 rounded-lg">
                         <div className="text-white text-2xl mx-auto mt-12">Please enter your Hop Token</div>
+                        <label htmlFor="hop-token" className="text-white text-sm mx-auto mt-12">
+                            Hop token
+                        </label>
+                        <input
+                            type="text"
+                            id="hop-token"
+                            className="bg-transparent text-white border-white/25 border rounded-md w-1/2 mx-auto mt-2"
+                        />
+                        <div className="mx-auto mt-4 cursor-pointer">
+                            <DeployBtn />
+                        </div>
                     </div>
                 </Content>
             </Portal>
